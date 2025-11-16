078e84ab478e7c5e989b8d0f582ab2dec392893a
gabfreitasarts-a11y <gabfreitas.arts@gmail.com>
2025-11-16 13:44:58 -0300

    Update pay.js

--- pay.js
+++ pay.js
@@ -3,7 +3,6 @@ const pixKey = '14982098075';
 const qrCanvas = document.getElementById('qrCanvas');
 const pixKeyText = document.getElementById('pixKeyText');
 if(pixKeyText) pixKeyText.innerText = pixKey;
-
 const emv = "00020126360014BR.GOV.BCB.PIX0114+55" + pixKey + "52040000530398654051.005802BR5925SOCIAL REPORT PLATAFORMA6009SAO PAULO62070503***6304";
 if(window.QRCode && qrCanvas){
   QRCode.toCanvas(qrCanvas, emv, { width: 240 }, function (error) {
@@ -12,12 +11,10 @@ if(window.QRCode && qrCanvas){
     }
   });
 }
-
 const copyBtn = document.getElementById('copyPix');
 copyBtn && copyBtn.addEventListener('click', ()=>{
   navigator.clipboard.writeText(pixKey).then(()=> alert('Chave PIX copiada: ' + pixKey)).catch(()=> alert('Não foi possível copiar automaticamente. Copie manualmente: ' + pixKey));
 });
-
 const btnJa = document.getElementById('btnJaPaguei');
 btnJa && btnJa.addEventListener('click', ()=>{
   localStorage.setItem('sr_paid_demo','true');
